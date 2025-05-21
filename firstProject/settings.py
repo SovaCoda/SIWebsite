5064adbc01e9bdb17d0dbf07ced005053befb6ce
Conner Bell <chb263@msstate.edu>
2025-05-21 16:32:47 -0500

    Update settings.py

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b&jy0&(_zj80kybzsy5wtz4=*-2xp=!sds7e)**u@0t06ib!kt'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
